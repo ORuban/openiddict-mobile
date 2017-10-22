9e5f69abcc0d0cb9408d010c671a5168c22bb0e7
oruban <settlers.net@gmail.com>
2017-10-22 19:41:47 +0300

    add PKCE (Proof Key for Code Exchange)

--- src/appClient/app/AuthClient.js
+++ src/appClient/app/AuthClient.js
@@ -3,6 +3,7 @@ import React from 'react';
 import SafariView from 'react-native-safari-view';
 import QueryString from 'query-string';
 import { Linking } from 'react-native';
+import { sha256 } from 'react-native-sha256';
 
 export class AuthClient {
     
@@ -20,15 +21,32 @@ export class AuthClient {
         this.client_secret = 'ECF8D87D-F510-405A-96D7-4D989D177840';
         this.redirect_uri = 'openiddictmobilesample://signin-oidc';
 
-        this.nonce = 'nonce'; //todo: should be a random string
-        this.state = 'state'; //todo: should be a random string
+        this.nonce = 'nonce_random_value'; //todo: should be a random string
+        this.state = 'state_random_value'; //todo: should be a random string
+
+        // PKCE (Proof Key for Code Exchange)
+        // From "Proof Key for Code Exchange by OAuth Public Clients" RFC: https://tools.ietf.org/html/rfc7636
+        //  code verifier
+        //     A cryptographically random string that is used to correlate the
+        //     authorization request to the token request.
+    
+        //  code challenge
+        //     A challenge derived from the code verifier that is sent in the
+        //     authorization request, to be verified against later.
+    
+        //  code challenge method
+        //     A method that was used to derive code challenge.
+        this.code_challenge_method = 'S256';
+        this.code_verifier = 'code_verifier_random_value';
  
         this.handleUrl = this.handleUrl.bind(this);
      }
 
-    login(){
+    async login(){
         Linking.addEventListener('url', this.handleUrl);
         
+        const code_challenge = await this.getCodeChallenge(this.code_verifier);
+
         SafariView.show({
             url: [
                 this.base_provider_url+'connect/authorize',
@@ -37,6 +55,8 @@ export class AuthClient {
                 '&client_secret='+this.client_secret,
                 '&scope=offline_access',
                 '&redirect_uri='+this.redirect_uri,
+                '&code_challenge='+code_challenge,
+                '&code_challenge_method='+this.code_challenge_method,
                 // From: http://openid.net/specs/openid-connect-core-1_0.html#IDTokenValidation
                 // nonce: OPTIONAL. String value used to associate a Client session with an ID Token, and to mitigate replay attacks.
                 // The value is passed through unmodified from the Authentication Request to the ID Token. Sufficient entropy MUST be present in the nonce values used to prevent attackers from guessing values.
@@ -72,10 +92,10 @@ export class AuthClient {
             'client_secret': this.client_secret,
             'grant_type': 'authorization_code',
             'code': code,
-            'redirect_uri': this.redirect_uri
+            'redirect_uri': this.redirect_uri,
+            'code_verifier': this.code_verifier
         };
-      
-        
+              
         try {
             let rawResponse = await postForm(this.base_provider_url+'connect/token', body);
             let jsonResponse = await rawResponse.json();
@@ -85,6 +105,21 @@ export class AuthClient {
             console.log('There was an error' + err);
             // Handle exceptions
         }
+    }
+
+    async getCodeChallenge(code)
+    {
+        const hash = await sha256(code);
+        let hash_bytes = parseHexString(hash);
+    
+        let Buffer = require('buffer/').Buffer;
+        let b = new Buffer(hash_bytes);
+        let base64Hash = b.toString('base64')
+            .replace('+','-')
+            .replace('/','_')
+            .replace('=','');
+        
+        return base64Hash;
     }
 }
 
@@ -110,3 +145,14 @@ function parseUrl (str, opts) {
 		queryParams: QueryString.parse(QueryString.extract(str), opts)
 	};
 };
+
+function parseHexString(str) { 
+    var result = [];
+    let x = 0;
+    while (x < str.length) { 
+        result.push(parseInt(str.substring(x, x+2), 16));
+        x += 2;
+    }
+
+    return result;
+}
